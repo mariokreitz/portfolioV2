958a1ae59ca1c6220931f201656015af51ad38a7
mariokreitz <mario.kreitz@web.de>
2025-10-16 17:05:14 +0200

    feat(github): add GitHub API configuration

---
+++ src/app/constants/github.ts
@@ -0,0 +1,9 @@
+export const GITHUB = {
+    API_URL: 'https://api.github.com',
+    SUB_DIRECTORY: {
+        USER: 'user',
+        EMOJIS: 'emojis',
+        EVENTS: 'events',
+    },
+    PROFILE_NAME: 'mariokreitz',
+} as const;
