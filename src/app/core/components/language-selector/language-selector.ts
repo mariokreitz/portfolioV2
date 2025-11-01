e680e5e3d4eb50c72dc857cd62a81e67460c14a1
mariokreitz <mario.kreitz@web.de>
2025-11-01 23:39:39 +0100

    feat(language-selector): add language selector component

---
+++ src/app/core/components/language-selector/language-selector.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-language-selector',
+    imports: [],
+    templateUrl: './language-selector.html',
+    styleUrl: './language-selector.css',
+})
+export class LanguageSelector {
+    // private readonly translate: TranslateService = inject(TranslateService);
+    // private readonly router: Router = inject(Router);
+
+}
