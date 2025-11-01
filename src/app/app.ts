049254f9f65c09489a235a77c08fedfcf487a32a
mariokreitz <mario.kreitz@web.de>
2025-11-01 23:39:35 +0100

    refactor(app): extract available languages to a separate file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,8 @@
 import { DOCUMENT, registerLocaleData } from '@angular/common';
-import localeDe from '@angular/common/locales/de';
-import localeEn from '@angular/common/locales/en';
-import localeDeExtra from '@angular/common/locales/extra/de';
-import localeEnExtra from '@angular/common/locales/extra/en';
 import { Component, inject, type OnInit } from '@angular/core';
 import { ActivatedRoute, type Data } from '@angular/router';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
-import { take } from 'rxjs';
+import { availableLanguages } from './core/constants/availableLanguages';
 import type { LanguageCode, LocaleData } from './core/models/app-language';
 
 @Component({
@@ -18,25 +14,14 @@ import type { LanguageCode, LocaleData } from './core/models/app-language';
     styleUrl: './app.css',
 })
 export class App implements OnInit {
-    private readonly availableLanguages: Readonly<Record<LanguageCode, LocaleData>> = {
-        de: [
-            localeDe,
-            localeDeExtra,
-        ],
-        en: [
-            localeEn,
-            localeEnExtra,
-        ],
-    } as const;
-
     private readonly translate: TranslateService = inject(TranslateService);
     private readonly activatedRoute: ActivatedRoute = inject(ActivatedRoute);
     private readonly document: Document = inject(DOCUMENT);
 
     constructor() {
-        this.translate.addLangs(Object.keys(this.availableLanguages));
+        this.translate.addLangs(Object.keys(availableLanguages));
 
-        (Object.entries(this.availableLanguages) as Array<[ LanguageCode, LocaleData ]>).forEach(
+        (Object.entries(availableLanguages) as Array<[ LanguageCode, LocaleData ]>).forEach(
           ([ language, [ core, extra ] ]: [ LanguageCode, LocaleData ]) => {
               registerLocaleData(core, language, extra);
           },
@@ -44,10 +29,10 @@ export class App implements OnInit {
     }
 
     public ngOnInit() {
-        this.activatedRoute.data.pipe(take(1)).subscribe((data: Data) => {
-            const language: string = data['lang'] || this.translate.getBrowserLang() || 'en';
-            this.document.documentElement.lang = language;
-            this.translate.use(language);
-        });
+        const data: Data = this.activatedRoute.snapshot.data;
+        const language: string = data['lang'] || this.translate.getBrowserLang() || 'en';
+        this.document.documentElement.lang = language;
+        this.translate.use(language);
+
     }
 }
---
+++ src/app/core/constants/availableLanguages.ts
@@ -0,0 +1,18 @@
+import localeDe from '@angular/common/locales/de';
+import localeEn from '@angular/common/locales/en';
+import localeDeExtra from '@angular/common/locales/extra/de';
+import localeEnExtra from '@angular/common/locales/extra/en';
+import type { LanguageCode, LocaleData } from '../models/app-language';
+
+export const availableLanguages: Readonly<Record<LanguageCode, LocaleData>> = {
+    de: [
+        localeDe,
+        localeDeExtra,
+    ],
+    en: [
+        localeEn,
+        localeEnExtra,
+    ],
+} as const;
+
+export const availableLanguagesCode: Array<LanguageCode> = Object.keys(availableLanguages) as Array<LanguageCode>;
