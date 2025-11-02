5096eade52ba883e89953be12ec316703b7c3e10
mariokreitz <mario.kreitz@web.de>
2025-11-03 00:12:41 +0100

    feat(projects): pass projects data to project grid component

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Layout } from '../../core/layout/layout';
 import { PageHeadline } from '../../features/page-headline/page-headline';
 import { ProjectGrid } from '../../features/project-grid/project-grid';
+import { projects } from '../../shared/constants/project-items';
+import type { ProjectItem } from '../../shared/models/project-item';
 
 @Component({
     selector: 'app-projects',
@@ -14,5 +16,5 @@ import { ProjectGrid } from '../../features/project-grid/project-grid';
     styleUrl: './projects.css',
 })
 export class Projects {
-
+    protected readonly projects: ProjectItem[] = projects;
 }
