c165a5a629fd2a908f7236b2fe7784ea8f9abab5
mariokreitz <mario.kreitz@web.de>
2025-11-02 03:29:02 +0100

    refactor(lang-navigator): rename method and add navigation support

--- src/app/core/services/lang-navigator.ts
+++ src/app/core/services/lang-navigator.ts
@@ -1,15 +1,17 @@
 import { inject, Injectable } from '@angular/core';
 import { type Params, Router, UrlSegment, UrlTree } from '@angular/router';
-import { availableLanguagesCode } from '../constants/availableLanguages';
+import { TranslateService } from '@ngx-translate/core';
+import { availableLanguagesCode } from '../constants/available-languages';
 import type { LanguageCode } from '../models/app-language';
 
 @Injectable({
     providedIn: 'root',
 })
 export class LangNavigatorService {
+    protected translate: TranslateService = inject(TranslateService);
     private readonly router: Router = inject(Router);
 
-    public async navigateTo(language: LanguageCode): Promise<void> {
+    public async setLanguage(language: LanguageCode): Promise<void> {
         const urlTree: UrlTree = this.router.parseUrl(this.router.url);
 
         const segments: string[] = urlTree.root.children['primary']?.segments.map((s: UrlSegment): string => s.path) || [];
@@ -17,7 +19,7 @@ export class LangNavigatorService {
         const urlWithoutLang: string[] = availableLanguagesCode.includes(segments[0] as LanguageCode) ? segments.slice(1) : segments;
 
         const fragment: string | undefined = urlTree.fragment ?? undefined;
-        
+
         const queryParams: Params = urlTree.queryParams;
 
         const newUrl: string[] = language !== 'de' ? [
@@ -34,5 +36,9 @@ export class LangNavigatorService {
             queryParams,
             queryParamsHandling: 'merge',
         });
+    }
+
+    public async navigateTo(path: string): Promise<void> {
+        await this.router.navigate([ (this.translate.getCurrentLang() != 'de' ? this.translate.getCurrentLang() : '') + path ]);
     }
 }
