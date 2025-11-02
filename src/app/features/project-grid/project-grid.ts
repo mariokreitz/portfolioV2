d783b766d3c017c7cb1b808ad71d019a6fa28271
mariokreitz <mario.kreitz@web.de>
2025-11-02 23:27:49 +0100

    fix(translations): correct under construction messages

---
+++ src/app/features/project-grid/project-grid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-grid',
+  imports: [],
+  templateUrl: './project-grid.html',
+  styleUrl: './project-grid.css'
+})
+export class ProjectGrid {
+
+}
