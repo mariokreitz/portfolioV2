a76796f176bab368dd48cd874285e410d7ee7573
mariokreitz <mario.kreitz@web.de>
2025-11-02 23:20:36 +0100

    feat(under-construction): implement translation for hint

--- src/app/core/components/under-construction-hint/under-construction-hint.ts
+++ src/app/core/components/under-construction-hint/under-construction-hint.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-under-construction-hint',
-  imports: [],
-  templateUrl: './under-construction-hint.html',
-  styleUrl: './under-construction-hint.css'
+    selector: 'app-under-construction-hint',
+    imports: [
+        TranslatePipe,
+    ],
+    templateUrl: './under-construction-hint.html',
+    styleUrl: './under-construction-hint.css',
 })
 export class UnderConstructionHint {
-
 }
