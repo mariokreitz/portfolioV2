ce57f8505db3756ba9d86a36927bbb2d771c629a
mariokreitz <mario.kreitz@web.de>
2025-11-02 22:18:18 +0100

    refactor(layout): replace inline navigation with component

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,26 +1,14 @@
-import { Component, inject } from '@angular/core';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { TranslatePipe } from '@ngx-translate/core';
-import { LanguageSelector } from '../components/language-selector/language-selector';
-import { navigationLinks } from '../constants/navigation-links';
-import type { NavigationItem } from '../models/navigation-item';
-import { LangNavigatorService } from '../services/lang-navigator';
+import { Component } from '@angular/core';
+import { Navigation } from '../components/navigation/navigation';
 
 @Component({
     selector: 'app-layout',
     imports: [
-        FaIconComponent,
-        LanguageSelector,
-        TranslatePipe,
+        Navigation,
     ],
     templateUrl: './layout.html',
     styleUrl: './layout.css',
 })
 export class Layout {
-    protected readonly navigationLinks: NavigationItem[] = navigationLinks;
-    private readonly langNavigator: LangNavigatorService = inject(LangNavigatorService);
 
-    public navigateTo(link: string): void {
-        void this.langNavigator.navigateTo(link);
-    }
 }
