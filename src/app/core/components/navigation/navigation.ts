eda562993901adcc6b93384cb4c7bf4d8fc1896f
mariokreitz <mario.kreitz@web.de>
2025-11-02 22:28:32 +0100

    refactor(navigation): enable standalone component and optimize change detection

--- src/app/core/components/navigation/navigation.ts
+++ src/app/core/components/navigation/navigation.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { TranslatePipe } from '@ngx-translate/core';
 import { navigationLinks } from '../../constants/navigation-links';
@@ -8,6 +8,7 @@ import { LanguageSelector } from '../language-selector/language-selector';
 
 @Component({
     selector: 'app-navigation',
+    standalone: true,
     imports: [
         FaIconComponent,
         LanguageSelector,
@@ -15,10 +16,11 @@ import { LanguageSelector } from '../language-selector/language-selector';
     ],
     templateUrl: './navigation.html',
     styleUrl: './navigation.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Navigation {
-
     protected readonly navigationLinks: NavigationItem[] = navigationLinks;
+    
     private readonly langNavigator: LangNavigatorService = inject(LangNavigatorService);
 
     public navigateTo(link: string): void {
