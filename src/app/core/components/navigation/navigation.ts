8f8ca153f44ef27aeb25d914cff34ee782a83938
mariokreitz <mario.kreitz@web.de>
2025-11-02 22:18:10 +0100

    feat(navigation): add navigation component with links and language selector

---
+++ src/app/core/components/navigation/navigation.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { TranslatePipe } from '@ngx-translate/core';
+import { navigationLinks } from '../../constants/navigation-links';
+import type { NavigationItem } from '../../models/navigation-item';
+import { LangNavigatorService } from '../../services/lang-navigator';
+import { LanguageSelector } from '../language-selector/language-selector';
+
+@Component({
+    selector: 'app-navigation',
+    imports: [
+        FaIconComponent,
+        LanguageSelector,
+        TranslatePipe,
+    ],
+    templateUrl: './navigation.html',
+    styleUrl: './navigation.css',
+})
+export class Navigation {
+
+    protected readonly navigationLinks: NavigationItem[] = navigationLinks;
+    private readonly langNavigator: LangNavigatorService = inject(LangNavigatorService);
+
+    public navigateTo(link: string): void {
+        void this.langNavigator.navigateTo(link);
+    }
+}
