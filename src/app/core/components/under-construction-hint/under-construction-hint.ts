eb136d5f91f73239c9d646dc2807800aa7939fc0
mariokreitz <mario.kreitz@web.de>
2025-11-02 23:13:21 +0100

    feat(under-construction): add under construction hint component

---
+++ src/app/core/components/under-construction-hint/under-construction-hint.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-under-construction-hint',
+  imports: [],
+  templateUrl: './under-construction-hint.html',
+  styleUrl: './under-construction-hint.css'
+})
+export class UnderConstructionHint {
+
+}
