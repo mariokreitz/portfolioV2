2fb96d61c465ac9611a1df267396f88b334d2fe2
mariokreitz <mario.kreitz@web.de>
2025-11-03 00:12:45 +0100

    feat(projects): pass projects data to project grid component

--- src/app/features/project-grid/project-grid.ts
+++ src/app/features/project-grid/project-grid.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input, type InputSignal } from '@angular/core';
+import type { ProjectItem } from '../../shared/models/project-item';
 
 @Component({
-  selector: 'app-project-grid',
-  imports: [],
-  templateUrl: './project-grid.html',
-  styleUrl: './project-grid.css'
+    selector: 'app-project-grid',
+    imports: [],
+    templateUrl: './project-grid.html',
+    styleUrl: './project-grid.css',
 })
 export class ProjectGrid {
-
+    public readonly projects: InputSignal<ProjectItem[]> = input.required<ProjectItem[]>();
 }
