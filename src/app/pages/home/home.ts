1260775cd051cd847dd373ff4cc0a6ae4bbcf567
mariokreitz <mario.kreitz@web.de>
2025-11-03 01:13:38 +0100

    feat(home): add profile card for Mario Kreitz

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Layout } from '../../core/layout/layout';
 
 @Component({
     selector: 'app-home',
     imports: [
-        TranslatePipe,
         Layout,
+        NgOptimizedImage,
+        TranslatePipe,
     ],
     templateUrl: './home.html',
     styleUrl: './home.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Home {
 
