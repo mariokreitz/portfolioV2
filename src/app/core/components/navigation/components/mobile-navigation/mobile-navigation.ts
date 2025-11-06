6458107da1760bcf6c0e8a971d02280cae57b08d
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-06 22:58:56 +0100

    feat(navigation): implement mobile navigation component

--- src/app/core/components/navigation/components/mobile-navigation/mobile-navigation.ts
+++ src/app/core/components/navigation/components/mobile-navigation/mobile-navigation.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, input, type InputSignal, signal } from '@angular/core';
+import { FaIconComponent, type IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
+import { TranslatePipe } from '@ngx-translate/core';
+import { Icon } from '../../../../../shared/components/icon/icon';
+import type { NavigationItem } from '../../../../models/navigation-item';
+import { LangNavigatorService } from '../../../../services/lang-navigator';
+import { LanguageSelector } from '../../../language-selector/language-selector';
 
 @Component({
-  selector: 'app-mobile-navigation',
-  imports: [],
-  templateUrl: './mobile-navigation.html',
-  styleUrl: './mobile-navigation.css'
+    selector: 'app-mobile-navigation',
+    imports: [
+        Icon,
+        FaIconComponent,
+        LanguageSelector,
+        TranslatePipe,
+
+    ],
+    templateUrl: './mobile-navigation.html',
+    styleUrl: './mobile-navigation.css',
+    host: { class: 'sm:hidden block' },
+    changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
 export class MobileNavigation {
+    public readonly navigationLinks: InputSignal<NavigationItem[]> = input.required();
 
+    protected readonly faBars: IconDefinition = faBars;
+    protected readonly faXmark: IconDefinition = faXmark;
+
+    protected readonly menuOpen = signal(false);
+    protected readonly ariaExpanded = computed(() => this.menuOpen());
+
+    private readonly langNavigator: LangNavigatorService = inject(LangNavigatorService);
+
+    public toggleMenu(): void {
+        this.menuOpen.update(v => !v);
+    }
+
+    public closeMenu(): void {
+        this.menuOpen.set(false);
+    }
+
+    public navigateTo(link: string): void {
+        void this.langNavigator.navigateTo(link);
+        this.closeMenu();
+    }
 }
