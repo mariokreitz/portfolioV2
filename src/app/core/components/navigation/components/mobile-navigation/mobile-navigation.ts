d5f3e535061e06e7525fd0936bec6ce0d82b417d
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-06 21:54:34 +0100

    feat(navigation): add mobile navigation component

---
+++ src/app/core/components/navigation/components/mobile-navigation/mobile-navigation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-navigation',
+  imports: [],
+  templateUrl: './mobile-navigation.html',
+  styleUrl: './mobile-navigation.css'
+})
+export class MobileNavigation {
+
+}
