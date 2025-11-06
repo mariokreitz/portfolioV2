b368cb8a976c002218b6f11d9cc91de568a29828
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-06 21:53:29 +0100

    feat(navigation): add desktop navigation component

---
+++ src/app/core/components/navigation/components/desktop-navigation/desktop-navigation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desktop-navigation',
+  imports: [],
+  templateUrl: './desktop-navigation.html',
+  styleUrl: './desktop-navigation.css'
+})
+export class DesktopNavigation {
+
+}
