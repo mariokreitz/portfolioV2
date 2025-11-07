d374c3511cc6ed592e38c2bb93909c5a1e2e473a
mariokreitz <mario.kreitz@web.de>
2025-11-07 15:08:51 +0100

    feat(configs): add interface for featured projects configuration

---
+++ src/app/shared/models/configs.ts
@@ -0,0 +1,8 @@
+export interface FeaturedProjectsConfig {
+    titleKey: string;
+    descriptionKey: string;
+    enableAutoScroll: boolean;
+    showControls: boolean;
+    delayBetweenSlides: number;
+    showSlidebar: boolean;
+}
