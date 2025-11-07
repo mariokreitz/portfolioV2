db99e5fb0d0b261a41fe217dd5875f81f58fff38
mariokreitz <mario.kreitz@web.de>
2025-11-07 15:37:41 +0100

    feat(section-headline): update template and add translation support

--- src/app/shared/components/section-headline/section-headline.ts
+++ src/app/shared/components/section-headline/section-headline.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input, type InputSignal } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-section-headline',
-  imports: [],
-  templateUrl: './section-headline.html',
-  styleUrl: './section-headline.css'
+    selector: 'app-section-headline',
+    imports: [
+        TranslatePipe,
+    ],
+    templateUrl: './section-headline.html',
+    styleUrl: './section-headline.css',
 })
 export class SectionHeadline {
-
+    public readonly title: InputSignal<string> = input.required<string>();
 }
