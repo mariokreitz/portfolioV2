ac7c50e9189e1ab03f652345946aedf12c665dc5
mariokreitz <mario.kreitz@web.de>
2025-11-07 02:09:06 +0100

    feat(home): add featured projects section to home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,7 +1,10 @@
 import { NgOptimizedImage } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, type Signal, signal } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Layout } from '../../core/layout/layout';
+import { FeaturedProject } from '../../features/featured-project/featured-project';
+import { projects } from '../../shared/constants/project-items';
+import type { ProjectItem } from '../../shared/models/project-item';
 
 @Component({
     selector: 'app-home',
@@ -9,11 +12,18 @@ import { Layout } from '../../core/layout/layout';
         Layout,
         NgOptimizedImage,
         TranslatePipe,
+        FeaturedProject,
+
     ],
     templateUrl: './home.html',
     styleUrl: './home.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Home {
+    protected readonly projects: Signal<ProjectItem[]>;
 
+    constructor() {
+        const featuredProject = projects.filter(project => project.isFeatured);
+        this.projects = signal(featuredProject);
+    }
 }
