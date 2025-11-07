08b5ecfde819c7678745727de2ac6a667d938174
mariokreitz <mario.kreitz@web.de>
2025-11-07 15:33:41 +0100

    feat(section-headline): add section headline component

---
+++ src/app/shared/components/section-headline/section-headline.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-headline',
+  imports: [],
+  templateUrl: './section-headline.html',
+  styleUrl: './section-headline.css'
+})
+export class SectionHeadline {
+
+}
