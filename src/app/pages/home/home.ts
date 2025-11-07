94012f34398ac8a20a1899f6b389dcda81024b80
mariokreitz <mario.kreitz@web.de>
2025-11-07 15:08:36 +0100

    refactor(home): update featured projects configuration

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,16 +1,16 @@
-import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, type Signal, signal } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Layout } from '../../core/layout/layout';
 import { FeaturedProject } from '../../features/featured-project/featured-project';
+import { featuredProjectsConfig } from '../../shared/configs/featured-projects';
 import { projects } from '../../shared/constants/project-items';
+import type { FeaturedProjectsConfig } from '../../shared/models/configs';
 import type { ProjectItem } from '../../shared/models/project-item';
 
 @Component({
     selector: 'app-home',
     imports: [
         Layout,
-        NgOptimizedImage,
         TranslatePipe,
         FeaturedProject,
 
@@ -21,9 +21,10 @@ import type { ProjectItem } from '../../shared/models/project-item';
 })
 export class Home {
     protected readonly projects: Signal<ProjectItem[]>;
+    protected readonly featuredProjectsConfig: FeaturedProjectsConfig = featuredProjectsConfig;
 
     constructor() {
-        const featuredProject = projects.filter(project => project.isFeatured);
+        const featuredProject: ProjectItem[] = projects.filter((project: ProjectItem) => project.isFeatured);
         this.projects = signal(featuredProject);
     }
 }
