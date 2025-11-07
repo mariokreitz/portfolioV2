0b1896ac8cc94600a877a65f24a93828834be003
mariokreitz <mario.kreitz@web.de>
2025-11-07 15:33:12 +0100

    feat(page-headline): add page headline component

---
+++ src/app/shared/components/page-headline/page-headline.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-headline',
+  imports: [],
+  templateUrl: './page-headline.html',
+  styleUrl: './page-headline.css'
+})
+export class PageHeadline {
+
+}
