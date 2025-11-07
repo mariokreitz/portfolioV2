e5cc1d248f727aeec4aa33c571ca857f7bccde13
mariokreitz <mario.kreitz@web.de>
2025-11-07 15:08:42 +0100

    feat(featured-projects): add configuration for featured projects

---
+++ src/app/shared/configs/featured-projects.ts
@@ -0,0 +1,10 @@
+import type { FeaturedProjectsConfig } from '../models/configs';
+
+export const featuredProjectsConfig: FeaturedProjectsConfig = {
+    titleKey: 'app.features.featured-projects.title',
+    descriptionKey: 'app.features.featured-projects.description',
+    enableAutoScroll: true,
+    showControls: true,
+    delayBetweenSlides: 5000,
+    showSlidebar: true,
+};
