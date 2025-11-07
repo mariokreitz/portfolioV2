48dabf2d454890ac8e641ad75ea973c141a651a0
mariokreitz <mario.kreitz@web.de>
2025-11-07 02:09:23 +0100

    feat(navigation): add social links to navigation

--- src/app/core/constants/navigation-links.ts
+++ src/app/core/constants/navigation-links.ts
@@ -1,16 +1,25 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faHome } from '@fortawesome/free-regular-svg-icons/faHome';
 import { faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
 import type { NavigationItem } from '../models/navigation-item';
 
 export const navigationLinks: NavigationItem[] = [
     {
-        label: 'nav.home',
+        label: 'navigation.home',
         link: '/',
         icon: faHome,
     },
     {
-        label: 'nav.projects',
+        label: 'navigation.projects',
         link: '/projects',
         icon: faProjectDiagram,
+    },
+];
+
+export const socialLinks: NavigationItem[] = [
+    {
+        label: 'GitHub',
+        link: 'https://www.github.com/mariokreitz',
+        icon: faGithub,
     },
 ];
