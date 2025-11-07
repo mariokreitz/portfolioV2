396a6e1e0890301187b397fcf6581ffacee97b01
mariokreitz <mario.kreitz@web.de>
2025-11-07 15:08:45 +0100

    feat(featured-project): add config input for featured projects

--- src/app/features/featured-project/featured-project.ts
+++ src/app/features/featured-project/featured-project.ts
@@ -1,18 +1,16 @@
-import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input, type InputSignal } from '@angular/core';
-import { TranslatePipe } from '@ngx-translate/core';
+import type { FeaturedProjectsConfig } from '../../shared/models/configs';
 import type { ProjectItem } from '../../shared/models/project-item';
 
 @Component({
     selector: 'app-featured-project',
-    imports: [
-        TranslatePipe,
-        NgOptimizedImage,
-    ],
+    imports: [],
     templateUrl: './featured-project.html',
     styleUrls: [ './featured-project.css' ],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FeaturedProject {
     public readonly projects: InputSignal<ProjectItem[]> = input.required<ProjectItem[]>();
+    public readonly config: InputSignal<FeaturedProjectsConfig> = input.required();
+
 }
