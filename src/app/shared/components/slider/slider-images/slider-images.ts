044cab3be5dd8abb460b974b74c46f1aa4bb5189
mariokreitz <mario.kreitz@web.de>
2025-11-07 15:47:20 +0100

    feat(slider-images): add slider images component

---
+++ src/app/shared/components/slider/slider-images/slider-images.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-images',
+  imports: [],
+  templateUrl: './slider-images.html',
+  styleUrl: './slider-images.css'
+})
+export class SliderImages {
+
+}
