9239fb9d2a63af0f525e29b6e4b021b1194f0a37
mariokreitz <mario.kreitz@web.de>
2025-11-07 15:08:32 +0100

    feat(slider): add slider progress bar component

---
+++ src/app/features/featured-project/components/slider-progressbar/slider-progressbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-progressbar',
+  imports: [],
+  templateUrl: './slider-progressbar.html',
+  styleUrl: './slider-progressbar.css'
+})
+export class SliderProgressbar {
+
+}
