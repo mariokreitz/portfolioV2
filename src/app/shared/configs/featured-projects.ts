bc85b21985b76c6d3c64ca6d91c27691cbffdaa8
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-08 23:15:53 +0100

    feat(featured-projects): enable autoplay and progress bar

--- src/app/shared/configs/featured-projects.ts
+++ src/app/shared/configs/featured-projects.ts
@@ -3,8 +3,8 @@ import type { FeaturedProjectsConfig } from '../models/configs';
 export const featuredProjectsConfig: FeaturedProjectsConfig = {
     titleKey: 'app.features.featured-projects.title',
     descriptionKey: 'app.features.featured-projects.description',
-    enableAutoScroll: true,
+    enableAutoplay: true,
     showControls: true,
+    showProgressbar: true,
     delayBetweenSlides: 5000,
-    showSlidebar: true,
 };
