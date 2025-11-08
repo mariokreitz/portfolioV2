c1b0b95c3270db6a9f8788069ef89d0f902ebb5d
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-09 00:15:20 +0100

    feat(slider-controls): add slider controls component

---
+++ src/app/shared/components/slider/slider-controls/slider-controls.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-slider-controls',
+    imports: [],
+    templateUrl: './slider-controls.html',
+    styleUrl: './slider-controls.css',
+})
+export class SliderControls {
+
+}
