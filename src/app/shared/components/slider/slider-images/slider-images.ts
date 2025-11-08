0ecc7e15d7b0a89722b9d13ed9d6f62f936df47f
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-08 23:19:14 +0100

    feat(slider-images): add input signals for slider configuration

--- src/app/shared/components/slider/slider-images/slider-images.ts
+++ src/app/shared/components/slider/slider-images/slider-images.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input, type InputSignal } from '@angular/core';
+import type { ProjectItem } from '../../../models/project-item';
 
 @Component({
-  selector: 'app-slider-images',
-  imports: [],
-  templateUrl: './slider-images.html',
-  styleUrl: './slider-images.css'
+    selector: 'app-slider-images',
+    imports: [],
+    templateUrl: './slider-images.html',
+    styleUrl: './slider-images.css',
 })
 export class SliderImages {
+    public readonly autoPlay: InputSignal<boolean> = input.required<boolean>();
+    public readonly showControls: InputSignal<boolean> = input.required<boolean>();
+    public readonly showProgressBar: InputSignal<boolean> = input.required<boolean>();
+    public readonly slideDelay: InputSignal<number> = input.required<number>();
+    public readonly slides: InputSignal<ProjectItem[]> = input.required<ProjectItem[]>();
 
 }
