2488a93d0d6c07bc4daecd1e01f83dd2d7d21b33
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-09 01:16:59 +0100

    feat(slider-images): enhance slider layout and controls

--- src/app/shared/components/slider/slider-images/slider-images.ts
+++ src/app/shared/components/slider/slider-images/slider-images.ts
@@ -1,10 +1,14 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component, input, type InputSignal } from '@angular/core';
 import type { ProjectItem } from '../../../models/project-item';
+import { SliderControls } from '../slider-controls/slider-controls';
+import { SliderProgressbar } from '../slider-progressbar/slider-progressbar';
 
 @Component({
     selector: 'app-slider-images',
     imports: [
+        SliderControls,
+        SliderProgressbar,
         NgOptimizedImage,
     ],
     templateUrl: './slider-images.html',
@@ -16,6 +20,5 @@ export class SliderImages {
     public readonly showProgressBar: InputSignal<boolean> = input.required<boolean>();
     public readonly slideDelay: InputSignal<number> = input.required<number>();
     public readonly slides: InputSignal<ProjectItem[]> = input.required<ProjectItem[]>();
-
 }
 
