c01b45bcb9bf0d2a9cf9be134be6c51803d7dd5e
mariokreitz <mario.kreitz@web.de>
2025-11-09 07:21:54 +0100

    feat(footer): enhance footer with GitHub link and styling

--- src/app/core/components/footer/footer.ts
+++ src/app/core/components/footer/footer.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule, type IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
-  selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.html',
-  styleUrl: './footer.css'
+    selector: 'app-footer',
+    imports: [ FontAwesomeModule ],
+    templateUrl: './footer.html',
+    styleUrl: './footer.css',
 })
 export class Footer {
-
+    protected readonly faGithub: IconDefinition = faGithub;
 }
