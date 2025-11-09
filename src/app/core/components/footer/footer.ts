06fc37b4d84d5e2fb66d3d16cdf72fb9ee4adf28
mariokreitz <mario.kreitz@web.de>
2025-11-09 07:29:31 +0100

    feat(footer): add imprint navigation link and update styles

--- src/app/core/components/footer/footer.ts
+++ src/app/core/components/footer/footer.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { FontAwesomeModule, type IconDefinition } from '@fortawesome/angular-fontawesome';
+import { Component, inject } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { TranslatePipe } from '@ngx-translate/core';
+import { LangNavigatorService } from '../../services/lang-navigator';
 
 @Component({
     selector: 'app-footer',
-    imports: [ FontAwesomeModule ],
+    imports: [
+        FontAwesomeModule,
+        TranslatePipe,
+    ],
     templateUrl: './footer.html',
     styleUrl: './footer.css',
 })
 export class Footer {
-    protected readonly faGithub: IconDefinition = faGithub;
+    protected readonly faGithub = faGithub;
+
+    private readonly langNavigator = inject(LangNavigatorService);
+
+    async navigateTo(link: string): Promise<void> {
+        await this.langNavigator.navigateTo(link);
+    }
 }
