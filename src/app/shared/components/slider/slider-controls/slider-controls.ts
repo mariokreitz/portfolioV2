ad534ef1f394da1f7652d3c3b63871b941d8891a
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-09 02:20:17 +0100

    feat(slider-controls): implement bullet navigation for slides

--- src/app/shared/components/slider/slider-controls/slider-controls.ts
+++ src/app/shared/components/slider/slider-controls/slider-controls.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, type InputSignal, type Signal } from '@angular/core';
 
 @Component({
     selector: 'app-slider-controls',
     imports: [],
     templateUrl: './slider-controls.html',
     styleUrl: './slider-controls.css',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SliderControls {
+export class SliderControls<T extends any[] = any[]> {
+    public readonly slides: InputSignal<T> = input.required<T>();
+    public readonly slidesLength: Signal<number> = computed(() => this.slides().length);
 
+    public goToSlide(index: number): void {
+        // Implementation for navigating to a specific slide
+        console.log(index);
+    }
 }
