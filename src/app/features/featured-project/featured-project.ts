e8d755c2ea61718308461ebd075c662a4ce301c2
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-09 01:37:46 +0100

    feat(featured-project): enable slider controls and progress bar

--- src/app/features/featured-project/featured-project.ts
+++ src/app/features/featured-project/featured-project.ts
@@ -10,6 +10,7 @@ import type { ProjectItem } from '../../shared/models/project-item';
     ],
     templateUrl: './featured-project.html',
     styleUrls: [ './featured-project.css' ],
+    host: { class: 'block' },
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FeaturedProject {
