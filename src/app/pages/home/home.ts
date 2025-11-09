88846b86add5ab84cb7df82e109f636948e9cc02
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-09 06:35:26 +0100

    refactor(home): update layout structure and improve semantics

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, type Signal, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, type Signal, signal } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Layout } from '../../core/layout/layout';
+import { LangNavigatorService } from '../../core/services/lang-navigator';
 import { FeaturedProject } from '../../features/featured-project/featured-project';
 import { SectionHeadline } from '../../shared/components/section-headline/section-headline';
 import { featuredProjectsConfig } from '../../shared/configs/featured-projects';
@@ -24,9 +25,14 @@ import type { ProjectItem } from '../../shared/models/project-item';
 export class Home {
     protected readonly projects: Signal<ProjectItem[]>;
     protected readonly featuredProjectsConfig: FeaturedProjectsConfig = featuredProjectsConfig;
+    private readonly langNavigator: LangNavigatorService = inject(LangNavigatorService);
 
     constructor() {
         const featuredProject: ProjectItem[] = projects.filter((project: ProjectItem) => project.isFeatured);
         this.projects = signal(featuredProject);
+    }
+
+    public navigateTo(link: string): void {
+        void this.langNavigator.navigateTo(link);
     }
 }
