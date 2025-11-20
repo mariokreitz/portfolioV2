421c2df1e815b5813fea2c2284d7e3d6007d9e5e
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-20 23:09:15 +0100

    refactor(page-headline): update template and add input signals

--- src/app/shared/components/page-headline/page-headline.ts
+++ src/app/shared/components/page-headline/page-headline.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, type InputSignal } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
+import { cn } from '../../utils';
 
 @Component({
-  selector: 'app-page-headline',
-  imports: [],
-  templateUrl: './page-headline.html',
-  styleUrl: './page-headline.css'
+    selector: 'app-page-headline',
+    imports: [
+        TranslatePipe,
+    ],
+    templateUrl: './page-headline.html',
+    styleUrl: './page-headline.css',
 })
 export class PageHeadline {
+    public readonly titleKey: InputSignal<string> = input.required<string>();
+    public readonly class: InputSignal<string | undefined> = input<string>();
 
+    protected readonly headlineClasses = computed(() =>
+      cn('text-3xl md:text-4xl', this.class()),
+    );
 }
+
+
