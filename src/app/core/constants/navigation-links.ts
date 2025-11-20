43be0aa1de26514e80d98d778361355df0792524
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-20 23:31:24 +0100

    feat(navigation): add new navigation items and icon condition

--- src/app/core/constants/navigation-links.ts
+++ src/app/core/constants/navigation-links.ts
@@ -1,18 +1,18 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faHome } from '@fortawesome/free-regular-svg-icons/faHome';
-import { faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
 import type { NavigationItem } from '../models/navigation-item';
 
 export const navigationLinks: NavigationItem[] = [
     {
         label: 'navigation.home',
         link: '/',
-        icon: faHome,
     },
     {
         label: 'navigation.projects',
         link: '/projects',
-        icon: faProjectDiagram,
+    },
+    {
+        label: 'navigation.aboutMe',
+        link: '/about',
     },
 ];
 
