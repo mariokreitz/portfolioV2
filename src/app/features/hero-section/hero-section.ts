bebd8db2c5ece291f6c9af6f6c667d6c174a9d25
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-20 23:09:32 +0100

    feat(hero-section): integrate page headline component

--- src/app/features/hero-section/hero-section.ts
+++ src/app/features/hero-section/hero-section.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { PageHeadline } from '../../shared/components/page-headline/page-headline';
 
 @Component({
-  selector: 'app-hero-section',
-  imports: [],
-  templateUrl: './hero-section.html',
-  styleUrl: './hero-section.css',
+    selector: 'app-hero-section',
+    imports: [
+        PageHeadline,
+    ],
+    templateUrl: './hero-section.html',
+    styleUrl: './hero-section.css',
 })
 export class HeroSection {
 
