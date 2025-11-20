e3b3d428a2e83f046a2f165a95d848b0ccae51bb
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-21 00:47:26 +0100

    refactor(navigation): simplify navigation component structure

--- src/app/core/components/navigation/navigation.ts
+++ src/app/core/components/navigation/navigation.ts
@@ -1,13 +1,11 @@
-import { ChangeDetectionStrategy, Component, input, type InputSignal } from '@angular/core';
-import { navigationLinks, socialLinks } from '../../constants/navigation-links';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { navigationLinks } from '../../constants/navigation-links';
 import type { NavigationItem } from '../../models/navigation-item';
 import { DesktopNavigation } from './components/desktop-navigation/desktop-navigation';
 import { MobileNavigation } from './components/mobile-navigation/mobile-navigation';
-import type { NavigationClassesConfig } from './navigation-class-config';
 
 @Component({
     selector: 'app-navigation',
-    standalone: true,
     imports: [
         MobileNavigation,
         DesktopNavigation,
@@ -17,8 +15,5 @@ import type { NavigationClassesConfig } from './navigation-class-config';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Navigation {
-    public readonly classes: InputSignal<NavigationClassesConfig | undefined> = input<NavigationClassesConfig>();
-    public readonly rootWrapperClass: InputSignal<string | undefined> = input<string>();
     protected readonly navigationLinks: NavigationItem[] = navigationLinks;
-    protected readonly socialLinks: NavigationItem[] = socialLinks;
 }
