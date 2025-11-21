4cce6498639ed0c433e91b3ad8bf4115ece5b6f5
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-21 03:16:27 +0100

    refactor(routes): move not found route to the end

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,7 @@ const childRoutes: Routes = [
     { path: '', loadComponent: () => import('./pages/home/home').then((m) => m.Home) },
     { path: 'projects', loadComponent: () => import('./pages/projects/projects').then((m) => m.Projects) },
     { path: 'imprint', loadComponent: () => import('./pages/imprint/imprint').then((m) => m.Imprint) },
-    { path: '**', loadComponent: () => import('./pages/not-found/not-found').then((m) => m.NotFound) },
-  
+
 ];
 
 export const routes: Routes = [
@@ -13,5 +12,7 @@ export const routes: Routes = [
     { path: '', loadComponent: () => import('./app').then((m) => m.App), data: { lang: 'de' }, children: childRoutes },
     //Englisch Routes
     { path: 'en', loadComponent: () => import('./app').then((m) => m.App), data: { lang: 'en' }, children: childRoutes },
+    //Not Found Route
+    { path: '**', loadComponent: () => import('./pages/not-found/not-found').then((m) => m.NotFound) },
 
 ];
