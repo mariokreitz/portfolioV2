50879a87d80d7b7cab0baf35455761aea379e640
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-21 07:35:48 +0100

    refactor(layout): remove underConstructionHint component

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { Footer } from '../components/footer/footer';
 import { Navigation } from '../components/navigation/navigation';
-import { UnderConstructionHint } from '../components/under-construction-hint/under-construction-hint';
 
 @Component({
     selector: 'app-layout',
     imports: [
         Navigation,
-        UnderConstructionHint,
         Footer,
         TranslatePipe,
     ],
