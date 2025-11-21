d6cc7ce8b7159924dc8a926ac1b6b3a1725f67b2
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-21 04:21:17 +0100

    feat(home): add side marker component to home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Layout } from '../../core/layout/layout';
 import { HeroSection } from '../../features/hero-section/hero-section';
+import { SideMarker } from '../../features/side-marker/side-marker';
+import { Image } from '../../shared/components/image/image';
 
 @Component({
     selector: 'app-home',
     imports: [
         Layout,
         HeroSection,
+        SideMarker,
+        Image,
     ],
     templateUrl: './home.html',
     styleUrl: './home.css',
