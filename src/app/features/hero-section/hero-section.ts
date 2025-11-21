2307331a7a0c8f5a2c0e934f9586f6b0df9da125
Mario Kreitz <mario.kreitz@codequarter.de>
2025-11-21 02:49:19 +0100

    feat(hero): enhance hero section with subheadline and button

--- src/app/features/hero-section/hero-section.ts
+++ src/app/features/hero-section/hero-section.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { Button } from '../../shared/components/button/button';
+import { Image } from '../../shared/components/image/image';
 import { PageHeadline } from '../../shared/components/page-headline/page-headline';
+import { PageSubheadline } from '../../shared/components/page-subheadline/page-subheadline';
 
 @Component({
     selector: 'app-hero-section',
     imports: [
+        Image,
         PageHeadline,
+        PageSubheadline,
+        Button,
     ],
     templateUrl: './hero-section.html',
     styleUrl: './hero-section.css',
+    host: { 'class': 'relative w-full h-full flex' },
 })
 export class HeroSection {
 
